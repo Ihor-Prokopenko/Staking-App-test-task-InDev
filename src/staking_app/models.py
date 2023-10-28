5961210ac58f342a6260b73a895d754cfb863d3a
IhorProkopenko <swaghettiemonsta@gmail.com>
2023-10-28 12:44:14 +0300

    feat(staking_app): created initial models

--- src/staking_app/models.py
+++ src/staking_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserWallet(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    balance = models.DecimalField(max_digits=20, decimal_places=10)
+
+
+class UserPosition(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    pool = models.ForeignKey('StackingPool', on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=20, decimal_places=10)
+
+
+class StackingPool(models.Model):
+    name = models.CharField(max_length=255)
+    conditions = models.ForeignKey('PoolConditions', on_delete=models.CASCADE)
+
+
+class PoolConditions(models.Model):
+    min_amount = models.DecimalField(max_digits=20, decimal_places=10)
+    max_amount = models.DecimalField(max_digits=20, decimal_places=10)
+
+
